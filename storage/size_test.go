83e0c8c9233bf9420606e778d0ebbca2b5aca8ab
chao77977 <gengchao77977@126.com>
2021-10-12 17:16:17 +0800

    more unittest

--- storage/size_test.go
+++ storage/size_test.go
@@ -12,3 +12,63 @@ func TestFormatByte(t *testing.T) {
 		t.Fatalf("Unexpected size %s != 1.00 GiB", f.Show())
 	}
 }
+
+func TestFormat(t *testing.T) {
+	x, err := Format(1024, "OiB")
+	if err == nil {
+		t.Fatalf("Unexpected size unit: OiB")
+	}
+
+	x, err = Format(1.01234567890, "GiB")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if x.Unit() != "GiB" {
+		t.Fatalf("Expected unit: GiB, got: %s", x.Unit())
+	}
+
+	size := x.Truncate(5)
+	if size != 1.01234 {
+		t.Fatalf("Expected size: 1.01234, got: %.5f", size)
+	}
+
+	size = x.Round(5)
+	if size != 1.01235 {
+		t.Fatalf("Expected size: 1.01235, got:i %.5f", size)
+	}
+}
+
+func TestCompare(t *testing.T) {
+	x, err := Format(2, "GiB")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	y, err := Format(4, "GiB")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if x.Compare(y) != -1 {
+		t.Fatalf("Unexpected compare %s > %s", x.Show(), y.Show())
+	}
+
+	z, err := Format(y.Sub(x))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if x.Compare(z) != 0 {
+		t.Fatalf("Unexpected compare %s != %s", x.Show(), z.Show())
+	}
+
+	x, err = Format(y.Add(z))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if x.Compare(y) != 1 {
+		t.Fatalf("Unexpected compare %s < %s", x.Show(), y.Show())
+	}
+}
